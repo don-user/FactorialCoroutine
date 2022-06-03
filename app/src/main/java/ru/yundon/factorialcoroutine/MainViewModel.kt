b2dc4c0a2d8294bb306bcbc76121b3befbeb888c
don-user <don_1303@mail.ru>
2022-06-03 10:12:34 +0300

    clean_project

--- app/src/main/java/ru/yundon/factorialcoroutine/MainViewModel.kt
+++ app/src/main/java/ru/yundon/factorialcoroutine/MainViewModel.kt
@@ -28,17 +28,6 @@ class MainViewModel: ViewModel() {
             _state.value = Error
             return
         }
-        //реализация без suspend и куорутины тут
-//        viewModelScope.launch {
-//            //выполняется на главном потоке
-//            val number = value.toLong()
-//            //подключаем поток Default
-//            val res = withContext(Dispatchers.Default){
-//                factorial(number)
-//            }
-//            //выполняется на главном потоке
-//            _state.value = FactorialResult(res)
-//        }
 
         //выполнение на собственной корутин коупе
         coroutineScope.launch {
@@ -52,13 +41,6 @@ class MainViewModel: ViewModel() {
             _state.value = FactorialResult(res)
             Log.d("MyTag", "ViewModel ${coroutineContext.toString()}")
         }
-
-    //реализация многопоточнити, где корутина в функции factorial()
-//        viewModelScope.launch {
-//            val number = value.toLong()
-//            val res = factorial(number)
-//            _state.value = FactorialResult(res)
-//        }
     }
 
     //реализация без suspend
@@ -76,28 +58,4 @@ class MainViewModel: ViewModel() {
     }
 
 
-    //реализация с использование многопоточность корутин (withContext)
-    //BigInteger - очень большие цифры, рабта с ними отличается от работы с Int
-//    private suspend fun factorial(number: Long) : String {
-//        return withContext(Dispatchers.Default){
-//            var result = BigInteger.ONE
-//            for (i in 1..number) {
-//                result = result.multiply(BigInteger.valueOf(i))
-//            }
-//            result.toString()
-//        }
-//    }
-
-    //suspendCoroutine используется когда реализованн коллбэк (асинхронный поток без корутин) с использование thread
-//    private suspend fun factorial(number: Long) : String{
-//        return suspendCoroutine {
-//            thread {
-//                var result = BigInteger.ONE
-//                for (i in 1..number){
-//                    result = result.multiply(BigInteger.valueOf(i))
-//                }
-//                it.resumeWith(Result.success(result.toString()))
-//            }
-//        }
-//    }
 }
